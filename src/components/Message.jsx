252b921b3a55842c9e063307cb74cecc419c8979
Yassinmoh <yassin_mohamed310@outlook.com>
2022-09-13 13:58:26 +0200

    create Messages Component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className='message owner'>
+            <div className="messageInfo">
+                <img src="https://images.pexels.com/photos/35537/child-children-girl-happy.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>Hello</p>
+                <img src="https://images.pexels.com/photos/35537/child-children-girl-happy.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            </div>
+        </div>
     )
 }
 
