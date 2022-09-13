33aa1551f40887f3c4acddcc46c6b215c68c4519
Yassinmoh <yassin_mohamed310@outlook.com>
2022-09-13 20:08:38 +0200

    set search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import avatar from '../img/avatar.png'
-
-
+import React, { useState } from 'react'
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../firebase'
 
 const Search = () => {
+
+    const [username, setUserName] = useState('')
+    const [user, setUser] = useState(null)
+    const [err, setErr] = useState(false)
+
+    // const { currentUser } = useContext(AuthContext);
+
+    const handleSearch = async () => {
+        const q = query(
+            collection(db, "users"),
+            where("displayName", "==", username)
+        );
+
+        try {
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                setUser(doc.data());
+            });
+        } catch (err) {
+            setErr(true);
+        }
+    };
+
+    const handleKey = (e) => {
+        e.code === "Enter" && handleSearch();
+    };
     return (
         <div className='search'>
             <div className="searchForm">
-                <input type="text" placeholder='find user'/>
+                <input type="text" placeholder='find user' onKeyDown={handleKey} onChange={(e) => setUserName(e.target.value)} />
             </div>
-            <div className="userChat">
-                <img src={avatar} alt="" />
+            {err && <span>User Not Found or somthing wrong</span>}
+            {user && <div className="userChat">
+                <img src={user.phtoURL} alt="" />
                 <div className="userChatInfo">
-                    <span>yassin</span>
+                    <span>{user.displayName}</span>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
