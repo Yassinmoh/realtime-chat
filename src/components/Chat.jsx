f27604e1db2eae1634496e8f536ebc5c441a4ea0
Yassinmoh <yassin_mohamed310@outlook.com>
2022-09-13 13:36:21 +0200

    create Input component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import '../styles/home.css'
-
+import Messages from './Messages'
+import Input from './Input'
+import Cam from '../img/cam.png'
+import Add from '../img/add.png'
+import More from '../img/more.png'
 
 
 const Chat = () => {
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className="chatInfo">
+                <span>yassin</span>
+                <div className="chatIcons">
+                    <img src={Cam} alt="" />
+                    <img src={Add} alt="" />
+                    <img src={More} alt="" />
+                </div>
+            </div>
+                <Messages />
+                <Input />
+        </div>
     )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Img from '../img/img.png'
+import Attach from '../img/attach.png'
+
 
 const Input = () => {
     return (
-        <div>Input</div>
+        <div className='input'>
+            <input type="text"  placeholder='message'/>
+            <div className="send">
+                <img src={Attach} alt="" />
+                <input type="file" id='file' style={{display:'none'}} />
+                <label htmlFor="file">
+                    <img src={Img} alt="" />
+                </label>
+                <button>Send</button>
+            </div>
+        </div>
     )
 }
 
