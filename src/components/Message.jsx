086911c2211d527c9332834bc2f5063348595b1e
Yassinmoh <yassin_mohamed310@outlook.com>
2022-09-15 13:12:02 +0200

    create readme file

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,22 @@
 import userEvent from '@testing-library/user-event'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { AuthContext } from '../context/authContext'
 import { ChatContext } from '../context/chatContext'
 
 const Message = ({message }) => {
-
+    const ref = useRef()
     const {currentUser} =useContext(AuthContext)
     const {data} =useContext(ChatContext)
+    const date = new Date()
+
+    useEffect(()=>{
+        ref.current?.scrollIntoView({behavior:'smooth'})
+    },[message])
     return (
-        <div className={`message ${message.senderId === currentUser.uid && 'owner'}`}>
+        <div className={`message ${message.senderId === currentUser.uid && 'owner'}`} ref={ref}>
             <div className="messageInfo">
                 <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
-                <span>just now</span>
+                <span style={{fontSize:'14px' , marginTop:'5px'}}>{date.toLocaleTimeString()}</span>
             </div>
             <div className="messageContent">
                 { message.text &&<p>{message.text}</p>}
